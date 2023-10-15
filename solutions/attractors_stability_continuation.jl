cf7f8c8395a3c42f56f5e70c9db42db662a3b48e
George Datseris <datseris.george@gmail.com>
2023-10-15 17:46:04 +0100

    Finish Attractors.jl lecture (#16)
    * finish DynSysintro: add exercises for GUI
    
    * better exercise for dynamical systems intro
    
    * correct exercise for Attractors lecture
    
    * better segment the dynsys tutorial
    
    * finish everything
    
    * run all codeblocks

--- solutions/attractors_stability_continuation.jl
+++ solutions/attractors_stability_continuation.jl
@@ -1,4 +1,6 @@
 # %% basins of attraction of multistable predator prey
+using DynamicalSystems
+
 function predator_prey_rule(u, p, t)
     r, c, μ, ν, α, β, χ, δ = p
     N, P = u
@@ -9,73 +11,102 @@ function predator_prey_rule(u, p, t)
 end
 
 u0 = SVector(8.0, 0.01)
-r1, r2 = 1.0, 2.0
 # r, c, μ, ν, α, β, χ, δ = p
-p = [r1, 0.19, 0.03, 0.003, 800, 1.5, 0.004, 2.2]
+p = [2.0, 0.19, 0.03, 0.003, 800, 1.5, 0.004, 2.2]
 
-using OrdinaryDiffEq: Tsit5
-diffeq = (alg = Tsit5(), adaptive = false, dt = 0.05)
+using OrdinaryDiffEq: Rodas5P
+diffeq = (alg = Rodas5P(), abstol = 1e-9, rtol = 1e-9)
 ds = CoupledODEs(predator_prey_rule, u0, p; diffeq)
 
-density = 201
-xg = range(0, 20; length = density)
-yg = range(0, 0.03; length = density)
+density = 101
+xg = range(-0.1, 20; length = density)
+yg = range(-0.0001, 0.03; length = density)
 grid = (xg, yg)
 
-colors = COLORS = [
-    "#7143E0",
-    "#191E44",
-    "#0A9A84",
-]
+basinsgrid = (xg[2:end], yg[2:end])
 
-fig = Figure(resolution = (800, 400))
-ax1 = Axis(fig[1,1]; title = "r = $(r1)", xlabel = "x", ylabel = "y")
-ax2 = Axis(fig[1,2]; title = "r = $(r2)", xlabel = "x", yticklabelsvisible = false)
+mapper = AttractorsViaRecurrences(
+    ds, grid;
+    # Estimation fails if `consecutive_recurrences` is too low, e.g., 10
+    # this happens because near y=0 the trajectory slows down very much.
+    # For this Δt value, this means that several integration steps are taken within
+    # the _same_ cell of the grid. Hence, the algorithm counts this as recurrences.
+    # 10 recurrences are very easy to accumulate in this way, and when this happens,
+    # the algorithm switches to "new attractor found" mode and proceeds to (incorrectly)
+    # identify some random cells near y = 0 as attractor cells.
+    consecutive_recurrences = 1000, Δt = 0.01,
+)
 
-for (j, r) in enumerate((r1, r2))
-    set_parameter!(ds, 1, r)
-    mapper = AttractorsViaRecurrences(
-        ds, grid; mx_chk_fnd_att = 2000, mx_chk_loc_att = 4000
-    )
-    ax = (ax1, ax2)[j]
-    basins, attractors = basins_of_attraction(mapper, grid)
-    heatmap!(ax, xg, yg, basins; colormap = colors)
-    for k ∈ keys(attractors)
-        x, y = columns(attractors[k])
-        scatter!(ax, vec(attractors[k]);
-            color = COLORS[k], markersize = 20,
-            strokewidth = 3, strokecolor = :white
-        )
-    end
-end
+basins, attractors = basins_of_attraction(mapper, basinsgrid)
+
+fig = heatmap_basins_attractors(basinsgrid, basins, attractors)
 
 fig
 
 # %% continuation of multistable predator prey
+sampler, = statespace_sampler(basinsgrid, 12345)
+
 mapper = AttractorsViaRecurrences(
-    ds, grid; mx_chk_fnd_att = 2000, mx_chk_loc_att = 4000
+    ds, grid;
+    consecutive_recurrences = 5000, Δt = 0.1,
+    force_non_adaptive = true, Ttr = 100.0,
 )
 
-sampler, = statespace_sampler(
-    min_bounds = minimum.(grid), max_bounds = maximum.(grid),
-)
-
-rsc = RecurrencesSeededContinuation(mapper)
+rafm = RecurrencesFindAndMatch(mapper)
 
 pidx = 1 # index of parameter to change (here Kd)
 prange = range(1, 2; length = 101) # parameter range to scan
 
 fractions_curves, attractors_info = continuation(
-    rsc, prange, pidx, sampler;
+    rafm, prange, pidx, sampler;
     show_progress = true, samples_per_parameter = 100
 )
 
-include(
-    joinpath(pathof(Attractors), "..", "..", "docs", "basins_plotting.jl")
-)
-
+# %%
 # Decide how to plot attractors: go from attractor to real number
 using Statistics: mean
-attractor_to_real = A -> mean((x[1] + x[2])/2 for x in A)
+a2r = A -> mean(x[1] for x in A)
 
-basins_attractors_curves_plot(fractions_curves, attractors_info, attractor_to_real, prange)
+plot_basins_attractors_curves(fractions_curves, attractors_info, a2r, prange)
+
+# Now analyze to find when the limit cycle becomes large enough
+# to be detected by our grid resolution
+id = 2 # id of the periodic attractor (from plot)
+j = findfirst(atts -> length(atts[id]) > 1, attractors_info)
+p_hopf = prange[j]
+
+# %% MFS of Thomas cyclical
+using DynamicalSystems
+
+function thomas_rule(u, p, t)
+    x,y,z = u
+    b = p[1]
+    xdot = sin(y) - b*x
+    ydot = sin(z) - b*y
+    zdot = sin(x) - b*z
+    return SVector(xdot, ydot, zdot)
+end
+
+thomas = CoupledODEs(thomas_rule, ones(3), [0.16])
+xg = yg = zg = range(-6.0, 6.0; length = 101)
+
+grid = (xg, yg, zg)
+
+mapper = AttractorsViaRecurrences(thomas, grid)
+
+id = mapper([1, 2, 3.0])
+A = extract_attractors(mapper)[id]
+
+mfs_algo = MFSBruteForce(1000, 1000, 0.99)
+search_area = (-6.0, 6.0)
+shocks = map(u0 -> minimal_fatal_shock(mapper, u0, search_area, mfs_algo), A)
+
+using CairoMakie
+using LinearAlgebra: norm
+using Statistics: mean
+
+shock_norms = norm.(shocks)
+mean_shock = mean(shocks)
+mean_norm = mean(shock_norms)
+
+scatter(columns(A)...; color = shock_norms)
